edc45944278c1d817259bfa6b1e293d0ce131cf6
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-07 22:55:08 +0300

    get ISS Longitude & Latitude

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
 from datetime import datetime
 import requests
 
+# My Longitude & Latitude
 LAT = 31.945368
 LONG = 35.928371
+
+# Get ISS location
+res = requests.get("http://api.open-notify.org/iss-now.json")
+res.raise_for_status()
+iss_data = res.json()
+
+iss_lat = float(iss_data["iss_position"]["latitude"])
+iss_long = float(iss_data["iss_position"]["longitude"])
+
+# Get Sunrise and Sunset in Amman (local timezone)
 PARAMS = {"lat": LAT, "lng": LONG, "formatted": 0}
 
 response = requests.get("https://api.sunrise-sunset.org/json", params=PARAMS)
