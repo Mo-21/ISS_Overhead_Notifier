c0f273371cf6e5677f5f81cbc69ee0ee7d05a159
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-07 22:48:13 +0300

    get current hour

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from datetime import datetime
 import requests
 
 LAT = 31.945368
@@ -9,3 +10,7 @@ data = response.json()
 
 sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
 sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
+
+now = str(datetime.now()).split(" ")[1].split(":")[0]
+
+print(now)
