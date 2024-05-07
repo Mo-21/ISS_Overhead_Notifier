c69fde07747c4e7a22cff3374ad53db0403efe23
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-07 22:48:35 +0300

    catch errors

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ LONG = 35.928371
 PARAMS = {"lat": LAT, "lng": LONG, "formatted": 0}
 
 response = requests.get("https://api.sunrise-sunset.org/json", params=PARAMS)
+response.raise_for_status()
 data = response.json()
 
 sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
