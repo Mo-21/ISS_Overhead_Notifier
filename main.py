22f37bc256783e4ec09eab9989233068fd89957b
Mohammad Malaeb <m.malaeb21@gmail.com>
2024-05-07 22:45:27 +0300

    get sunrise & sunset hours only

--- main.py
+++ main.py
@@ -2,9 +2,10 @@ import requests
 
 LAT = 31.945368
 LONG = 35.928371
-PARAMS = {"lat": LAT, "lng": LONG}
+PARAMS = {"lat": LAT, "lng": LONG, "formatted": 0}
 
 response = requests.get("https://api.sunrise-sunset.org/json", params=PARAMS)
 data = response.json()
 
-print(data)
+sunrise = data["results"]["sunrise"].split("T")[1].split(":")[0]
+sunset = data["results"]["sunset"].split("T")[1].split(":")[0]
